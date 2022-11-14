29918948e95e1998e67c7026e631dd5e64597b6f
Hai M Nguyen <vnjp22@gmail.com>
2022-11-14 15:43:20 -0800

    Export Context, HeaderStyles and more components

--- src/modules.js
+++ src/modules.js
@@ -1,2 +1,15 @@
-export VuiTheme from "assets/theme";
+export * as Context from 'context';
+export * as HeaderStyles from "examples/Navbars/DashboardNavbar/styles";
+
+export VuiTheme from 'assets/theme';
+
+export VuiAlert from 'components/VuiAlert';
+export VuiAvatar from 'components/VuiAvatar';
+export VuiBadge from 'components/VuiBadge';
+export VuiBox from 'components/VuiBox';
 export VuiButton from 'components/VuiButton';
+export VuiInput from 'components/VuiInput';
+export VuiPagination from 'components/VuiPagination';
+export VuiProgress from 'components/VuiProgress';
+export VuiSwitch from 'components/VuiSwitch';
+export VuiTypography from 'components/VuiTypography';
