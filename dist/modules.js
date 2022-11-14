31ab066c6066601e6628817140927a608820e7b6
Hai M Nguyen <vnjp22@gmail.com>
2022-11-14 15:43:29 -0800

    update dist

--- dist/modules.js
+++ dist/modules.js
@@ -3,10 +3,59 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.HeaderStyles = exports.Context = void 0;
+Object.defineProperty(exports, "VuiAlert", {
+  enumerable: true,
+  get: function () {
+    return _VuiAlert.default;
+  }
+});
+Object.defineProperty(exports, "VuiAvatar", {
+  enumerable: true,
+  get: function () {
+    return _VuiAvatar.default;
+  }
+});
+Object.defineProperty(exports, "VuiBadge", {
+  enumerable: true,
+  get: function () {
+    return _VuiBadge.default;
+  }
+});
+Object.defineProperty(exports, "VuiBox", {
+  enumerable: true,
+  get: function () {
+    return _VuiBox.default;
+  }
+});
 Object.defineProperty(exports, "VuiButton", {
   enumerable: true,
   get: function () {
     return _VuiButton.default;
+  }
+});
+Object.defineProperty(exports, "VuiInput", {
+  enumerable: true,
+  get: function () {
+    return _VuiInput.default;
+  }
+});
+Object.defineProperty(exports, "VuiPagination", {
+  enumerable: true,
+  get: function () {
+    return _VuiPagination.default;
+  }
+});
+Object.defineProperty(exports, "VuiProgress", {
+  enumerable: true,
+  get: function () {
+    return _VuiProgress.default;
+  }
+});
+Object.defineProperty(exports, "VuiSwitch", {
+  enumerable: true,
+  get: function () {
+    return _VuiSwitch.default;
   }
 });
 Object.defineProperty(exports, "VuiTheme", {
@@ -15,6 +64,27 @@ Object.defineProperty(exports, "VuiTheme", {
     return _theme.default;
   }
 });
+Object.defineProperty(exports, "VuiTypography", {
+  enumerable: true,
+  get: function () {
+    return _VuiTypography.default;
+  }
+});
+var _Context = _interopRequireWildcard(require("./context"));
+exports.Context = _Context;
+var _HeaderStyles = _interopRequireWildcard(require("./examples/Navbars/DashboardNavbar/styles"));
+exports.HeaderStyles = _HeaderStyles;
 var _theme = _interopRequireDefault(require("./assets/theme"));
+var _VuiAlert = _interopRequireDefault(require("./components/VuiAlert"));
+var _VuiAvatar = _interopRequireDefault(require("./components/VuiAvatar"));
+var _VuiBadge = _interopRequireDefault(require("./components/VuiBadge"));
+var _VuiBox = _interopRequireDefault(require("./components/VuiBox"));
 var _VuiButton = _interopRequireDefault(require("./components/VuiButton"));
+var _VuiInput = _interopRequireDefault(require("./components/VuiInput"));
+var _VuiPagination = _interopRequireDefault(require("./components/VuiPagination"));
+var _VuiProgress = _interopRequireDefault(require("./components/VuiProgress"));
+var _VuiSwitch = _interopRequireDefault(require("./components/VuiSwitch"));
+var _VuiTypography = _interopRequireDefault(require("./components/VuiTypography"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
